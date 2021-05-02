9f95cbc5bfd68120b1478e39d54709466301f3da
Denver Bohling <denver.bohling@gmail.com>
2021-05-02 00:15:54 -0700

    adds routing

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { GalleryContainerComponent } from './shared/gallery/gallery-container/gallery-container.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: GalleryContainerComponent
+    redirectTo: '/gallery/all',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gallery',
+    children: [
+      {
+        path: 'all',
+        component: GalleryContainerComponent
+      },
+      {
+        path: 'sly',
+        component: GalleryContainerComponent
+      },
+      {
+        path: 'essie',
+        component: GalleryContainerComponent
+      }
+    ]
   }
 ];
 
