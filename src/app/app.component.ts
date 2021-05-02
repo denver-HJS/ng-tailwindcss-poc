03f925ee0d40e3ec8d4a531d8105b7f399ec045a
Denver Bohling <denver.bohling@gmail.com>
2021-05-01 23:43:38 -0700

    add some responsive design to the sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  static readonly desktopBreakpoints = [Breakpoints.XLarge, Breakpoints.Large, Breakpoints.Medium];
+
   title = 'ng-tailwindcss-poc';
   @ViewChild(MatSidenav) sidenav: MatSidenav;
-  isOpened: boolean = false;
-  sideNavMode: 'side' | 'over' = 'side';
+  sideNavMode$: BehaviorSubject<'side' | 'over'>;
 
   constructor(private readonly breakpoints$: BreakpointObserver) {
+    this.sideNavMode$ = new BehaviorSubject<'side' | 'over'>('side');
   }
 
-  ngOnInit() {
-    this.breakpoints$.observe([Breakpoints.XLarge, Breakpoints.Large, Breakpoints.Medium])
-      .subscribe(async (event: BreakpointState) => {
-        if (event.matches) {
-          this.sideNavMode = 'side';
+  async ngOnInit() {
+    this.breakpoints$.observe(AppComponent.desktopBreakpoints)
+      .subscribe(async ( desktopBreakpointEvent: BreakpointState) => {
+        if (desktopBreakpointEvent.matches) {
+          this.sideNavMode$.next('side');
           await this.sidenav.open();
         } else {
-          this.sideNavMode = 'over';
+          this.sideNavMode$.next('over');
           await this.sidenav.close();
         }
       });
+  }
+
+  async ngAfterViewInit() {
+    await this.sidenav.open();
   }
 }
